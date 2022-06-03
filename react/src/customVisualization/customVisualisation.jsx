6f05a508a395429b10f49479ba43f0138fca859f
Martina <hunfeld@consist.de>
2022-06-03 15:07:44 +0200

    dynamic data from search

--- react/src/customVisualization/customVisualisation.jsx
+++ react/src/customVisualization/customVisualisation.jsx
@@ -16,84 +16,58 @@ L.Icon.Default.mergeOptions({
 });
 
 
-const position = [51.505, -0.09]
+const position = [51.1642292, 10.4541194] 
 
 
-// // Convert sparkline data from the datasource a format usable by the @splunk/react-sparkline component
-// const formatSparklineData = (data) => {
-//     return data.slice(1).map((val) => parseInt(val, 10));
-// };
+// Extract data from the datasource a format usable by the table
+const formatData = (dataSources) => {
+    if (!dataSources.primary.data) {
+        return {
+            fields: [],
+            data: [],
+        };
+    }
 
-// // Extract data from the datasource a format usable by the table
-// const formatData = (dataSources) => {
-//     if (!dataSources.primary.data) {
-//         return {
-//             fields: [],
-//             data: [],
-//         };
-//     }
+    // Get the names of the fields
+    const fields = dataSources.primary.data.fields.map((f) => f.name);
+    const data = [];
 
-//     // Get the names of the fields
-//     const fields = dataSources.primary.data.fields.map((f) => f.name);
-//     const data = [];
+    // Convert the data from column to row form
+    dataSources.primary.data.columns.forEach((col, i) => {
+        col.forEach((item, j) => {
+            if (!data[j]) {
+                data.push({});
+            }
+            data[j][fields[i].replace(/\s/g, '')] = item;
+        });
+    });
 
-//     // Convert the data from column to row form
-//     dataSources.primary.data.columns.forEach((col, i) => {
-//         col.forEach((item, j) => {
-//             if (!data[j]) {
-//                 data.push({});
-//             }
-//             data[j][fields[i].replace(/\s/g, '')] =
-//                 fields[i] === 'Logins Last Week'
-//                     ? formatSparklineData(item)
-//                     : item;
-//         });
-//     });
-
-//     return { fields, data };
-// };
+    console.log('data', dataSources.primary.data);
+    return { fields, data };
+};
 
 const LeafletMap = ({ options, dataSources }) => {
-    // const [tableData, setTableData] = useState(formatData(dataSources));
+    const [tableData, setTableData] = useState(formatData(dataSources));
 
-    // useEffect(() => {
-    //     setTableData(formatData(dataSources));
-    // }, [dataSources]);
-
-    // const handleRequestMoveRow = useCallback(
-    //     ({ fromIndex, toIndex }) => {
-    //         if (!tableData) {
-    //             return;
-    //         }
-    //         const data = cloneDeep(tableData.data);
-    //         const rowToMove = data[fromIndex];
-
-    //         const insertionIndex = toIndex < fromIndex ? toIndex : toIndex + 1;
-    //         data.splice(insertionIndex, 0, rowToMove);
-
-    //         const removalIndex =
-    //             toIndex < fromIndex ? fromIndex + 1 : fromIndex;
-    //         data.splice(removalIndex, 1);
-
-    //         setTableData({
-    //             fields: tableData.fields,
-    //             data,
-    //         });
-    //     },
-    //     [tableData]
-    // );
+    useEffect(() => {
+        setTableData(formatData(dataSources));
+    }, [dataSources]);
 
     return (
-        <MapContainer style={{ height: "100%", minHeight: "100%" }} center={position} zoom={13} scrollWheelZoom={false}>
+        <MapContainer style={{ height: "100%", minHeight: "100%" }} center={position} zoom={7} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
+            {tableData.data.map((row) => (
+            <Marker position={[row.latitude, row.longitude]}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    Name: {row.name} <br /> 
+                    latitude: {row.latitude} <br />
+                    longitude: {row.longitude} <br />
                 </Popup>
             </Marker>
+            ))}
         </MapContainer>
     );
 };
